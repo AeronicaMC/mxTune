8d5a0e54e7375804e141a9313e7043ddc4003352
Aeronica <Aeronica@users.noreply.github.com>
2016-07-22 19:34:54 -0500

    Auto update mod version in java source from gradle.

--- src/main/java/net/aeronica/mods/mxtune/MXTuneMain.java
+++ src/main/java/net/aeronica/mods/mxtune/MXTuneMain.java
@@ -40,7 +40,7 @@ public class MXTuneMain
 {
     public static final String MODID = "mxtune";
     public static final String MODNAME = "mxTune";
-    public static final String VERSION = "${version}";
+    public static final String VERSION = "{@version:mod}";
     public static final String GUIFACTORY = "net.aeronica.mods.mxtune.config.ModGuiFactory"; 
 
     @Mod.Instance(MODID)
