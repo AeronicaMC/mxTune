01881e2981c3ca7e5dcd3eef4a1136318e00cbe0
Aeronica <pmboese@gmail.com>
2017-02-19 12:23:01 -0600

    Eclipse Neon is messing with me. Put the ModEntities.init() back where it belongs.

--- src/main/java/net/aeronica/mods/mxtune/proxy/CommonProxy.java
+++ src/main/java/net/aeronica/mods/mxtune/proxy/CommonProxy.java
@@ -20,6 +20,7 @@ import net.aeronica.mods.mxtune.config.ModConfig;
 import net.aeronica.mods.mxtune.groups.GroupManager;
 import net.aeronica.mods.mxtune.handler.CommonEventHandler;
 import net.aeronica.mods.mxtune.init.ModBlocks;
+import net.aeronica.mods.mxtune.init.ModEntities;
 import net.aeronica.mods.mxtune.util.Recipes;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
@@ -72,7 +73,9 @@ public abstract class CommonProxy implements IProxy
         Recipes.register();
     }
 
-    public void initEntities() {}
+    public void initEntities() {
+        ModEntities.init();
+    }
     
     @Override
     public void initMML() {}
