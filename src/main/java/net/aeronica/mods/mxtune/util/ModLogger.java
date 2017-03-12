7b2315b0bf4037d5e2050601993dabaa0c895631
Aeronica <pmboese@gmail.com>
2017-03-12 13:30:04 -0500

    Experimental changes to simplify logging stacktraces.

--- src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
+++ src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
@@ -15,7 +15,17 @@ public class ModLogger {
     }
 
     public static void log(Level level, String format, Object... data) {
-        logger.printf(level, format, data);
+        for(Object obj : data)
+        {
+            if (obj instanceof Exception)
+            {
+                for(StackTraceElement ste : ((Exception) obj).getStackTrace())
+                {
+                    logger.printf(level, format, ste); 
+                }
+            } else
+                logger.printf(level, format, data);
+        }
     }
 
     public static void info(String format, Object... data){
@@ -38,4 +48,24 @@ public class ModLogger {
         log(Level.FATAL, format, data);
     }
 
+    public static void info(Object... data){
+        log(Level.INFO, "%s", data);
+    }
+    
+    public static void debug(Object... data){
+        log(Level.DEBUG, "%s", data);
+    }
+    
+    public static void warning(Object... data){
+        log(Level.WARN, "%s", data);
+    }
+
+    public static void error(Object... data){
+        log(Level.ERROR, "%s", data);
+    }
+
+    public static void fatal(Object... data){
+        log(Level.FATAL, "%s", data);
+    }
+    
 }
