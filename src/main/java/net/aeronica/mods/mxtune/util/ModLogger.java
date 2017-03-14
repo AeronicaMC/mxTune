624190e3305d52ebfb98a218d092dda928f9fad0
Aeronica <pmboese@gmail.com>
2017-03-13 21:57:37 -0500

    Add exception stack trace logging to the ModLogger.

--- src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
+++ src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
@@ -18,6 +18,20 @@ public class ModLogger {
         logger.printf(level, format, data);
     }
     
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at the specified log {@link level}
+     * @param level     The specified log level
+     * @param e         The raw exception 
+     */
+    public static <T extends Exception> void log(Level level, T e)
+    {
+        if (e != null) {
+            log(level, "%s", e.getLocalizedMessage());
+            for(StackTraceElement s: e.getStackTrace())
+                log(level, "%s", s.toString());
+        }
+    }
+    
     public static void info(String format, Object... data){
         log(Level.INFO, format, data);
     }
@@ -38,8 +52,49 @@ public class ModLogger {
         log(Level.FATAL, format, data);
     }
 
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.INFO
+     * @param e         The raw exception 
+     */
+    public static <T extends Exception> void info(T e)
+    {
+        log(Level.INFO, e);
+    }
+    
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.DEBUG
+     * @param e         The raw exception 
+     */
+    public static <T extends Exception> void debug(T e)
+    {
+        log(Level.DEBUG, e);
+    }
+    
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.WARM
+     * @param e         The raw exception 
+     */
+    public static <T extends Exception> void warning(T e)
+    {
+        log(Level.WARN, e);
+    }
+    
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.ERROR
+     * @param e         The raw exception 
+     */
     public static <T extends Exception> void error(T e)
     {
-        logger.error(e.getStackTrace());
+        log(Level.ERROR, e);
     }
+    
+    /**
+     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.FATAL
+     * @param e         The raw exception 
+     */
+    public static <T extends Exception> void fatal(T e)
+    {
+        log(Level.FATAL, e);
+    }
+    
 }
