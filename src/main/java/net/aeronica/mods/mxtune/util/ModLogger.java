abfa9c25b40f36b74d5137278fdcdb1204209c34
Aeronica <pmboese@gmail.com>
2018-09-02 18:50:39 -0500

    Update from Bard Mania

--- src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
+++ src/main/java/net/aeronica/mods/mxtune/util/ModLogger.java
@@ -1,100 +1,89 @@
 package net.aeronica.mods.mxtune.util;
 
+import net.aeronica.mods.mxtune.MXTuneMain;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.Marker;
+import org.apache.logging.log4j.MarkerManager;
 
-public class ModLogger {
-
+public class ModLogger
+{
+    private static final Marker MOD_MARKER = MarkerManager.getMarker(MXTuneMain.MODID);
     private static Logger logger;
 
-    public static void setLogger(Logger logger) {
-        if (ModLogger.logger != null) {
+    public static void setLogger(Logger logger)
+    {
+        if (ModLogger.logger != null)
+        {
             throw new IllegalStateException("Attempt to replace logger");
         }
         ModLogger.logger = logger;
     }
 
-    public static void log(Level level, String format, Object... data) {
-        logger.printf(level, format, data);
+    public static void log(Level level, final Marker marker, String format, Object... data)
+    {
+        logger.printf(level, marker, format, data);
     }
-    
+
     /**
-     * Log a stack trace for &lt;T extends Throwable&gt; {@link e} at the specified log {@link level}
-     * @param level     The specified log level
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at the specified log {@link Level}
+     *
+     * @param level The specified log level
+     * @param e     The raw exception
      */
     public static <T extends Throwable> void log(Level level, T e)
     {
-        if (e != null) {
-            log(level, "%s", e.getLocalizedMessage());
-            for(StackTraceElement s: e.getStackTrace())
-                log(level, "%s", s.toString());
+        if (e != null)
+        {
+            log(level, MOD_MARKER, "%s", e.getLocalizedMessage());
+            for (StackTraceElement s : e.getStackTrace())
+                log(level, MOD_MARKER, "%s", s.toString());
         }
     }
-    
-    public static void info(String format, Object... data){
-        log(Level.INFO, format, data);
-    }
-    
-    public static void debug(String format, Object... data){
-        log(Level.DEBUG, format, data);
-    }
-    
-    public static void warning(String format, Object... data){
-        log(Level.WARN, format, data);
-    }
 
-    public static void error(String format, Object... data){
-        log(Level.ERROR, format, data);
-    }
+    public static void info(String format, Object... data) {log(Level.INFO, MOD_MARKER, format, data);}
 
-    public static void fatal(String format, Object... data){
-        log(Level.FATAL, format, data);
-    }
+    public static void debug(String format, Object... data) {log(Level.DEBUG, MOD_MARKER, format, data);}
+
+    public static void warn(String format, Object... data) {log(Level.WARN, MOD_MARKER, format, data);}
+
+    public static void error(String format, Object... data) {log(Level.ERROR, MOD_MARKER, format, data);}
+
+    public static void fatal(String format, Object... data) {log(Level.FATAL, MOD_MARKER, format, data);}
 
     /**
-     * Log a stack trace for &lt;T extends Exception&gt; {@link e} at log Level.INFO
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at log Level.INFO
+     *
+     * @param e The raw exception
      */
-    public static <T extends Throwable> void info(T e)
-    {
-        log(Level.INFO, e);
-    }
-    
+    public static <T extends Throwable> void info(T e) {log(Level.INFO, e);}
+
     /**
-     * Log a stack trace for &lt;T extends Throwable&gt; {@link e} at log Level.DEBUG
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at log Level.DEBUG
+     *
+     * @param e The raw exception
      */
-    public static <T extends Throwable> void debug(T e)
-    {
-        log(Level.DEBUG, e);
-    }
-    
+    public static <T extends Throwable> void debug(T e) {log(Level.DEBUG, e);}
+
     /**
-     * Log a stack trace for &lt;T extends Throwable&gt; {@link e} at log Level.WARM
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at log Level.WARN
+     *
+     * @param e The raw exception
      */
-    public static <T extends Throwable> void warning(T e)
-    {
-        log(Level.WARN, e);
-    }
-    
+    public static <T extends Throwable> void warn(T e) {log(Level.WARN, e);}
+
     /**
-     * Log a stack trace for &lt;T extends Throwable&gt; {@link e} at log Level.ERROR
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at log Level.ERROR
+     *
+     * @param e The raw exception
      */
-    public static <T extends Throwable> void error(T e)
-    {
-        log(Level.ERROR, e);
-    }
-    
+    public static <T extends Throwable> void error(T e) {log(Level.ERROR, e);}
+
     /**
-     * Log a stack trace for &lt;T extends Throwable&gt; {@link e} at log Level.FATAL
-     * @param e         The raw exception 
+     * Log a stack trace for &lt;T extends Throwable&gt; {@link Throwable} at log Level.FATAL
+     *
+     * @param e The raw exception
      */
-    public static <T extends Throwable> void fatal(T e)
-    {
-        log(Level.FATAL, e);
-    }
-    
+    public static <T extends Throwable> void fatal(T e) {log(Level.FATAL, e);}
 }
+
