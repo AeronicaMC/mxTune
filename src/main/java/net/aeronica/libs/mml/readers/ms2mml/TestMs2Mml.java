3c19ead60b2b687ebd9b4098c53bc9c76f5af984
Aeronica <pmboese@gmail.com>
2019-01-06 19:13:20 -0600

    Open zip files and read contained ms2mml files. TEST ONLY.

--- src/main/java/net/aeronica/libs/mml/readers/ms2mml/TestMs2Mml.java
+++ src/main/java/net/aeronica/libs/mml/readers/ms2mml/TestMs2Mml.java
@@ -17,6 +17,7 @@
 
 package net.aeronica.libs.mml.readers.ms2mml;
 
+import net.aeronica.libs.mml.core.MMLAllowedCharacters;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,6 +26,11 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 
 public class TestMs2Mml
@@ -33,26 +39,28 @@ public class TestMs2Mml
 
     private static Logger LOGGER = LogManager.getLogger();
 
-    static void test() throws JAXBException
+    static void test(InputStream is) throws JAXBException
     {
         JAXBContext jaxbContext = JAXBContext.newInstance("net.aeronica.libs.mml.readers.ms2mml");
         Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-        Ms2 ms2 = (Ms2) unmarshaller.unmarshal(getFile());
+        Ms2 ms2 = (Ms2) unmarshaller.unmarshal(is);
 
-        LOGGER.info("Melody {}", ms2.getMelody().replace('\n', ' ').trim());
-        for (Ms2.Chord chord : ms2.chord)
-        {
-            LOGGER.info("Chord {}: {}", chord.index, chord.value.replace('\n', ' ').trim());
-        }
+        LOGGER.info("Melody {}", MMLAllowedCharacters.filterAllowedCharacters(ms2.melody));
+        if (ms2.chord != null)
+            for (Ms2.Chord chord : ms2.chord)
+            {
+                LOGGER.info("Chord {}: {}", chord.index, MMLAllowedCharacters.filterAllowedCharacters(chord.value));
+            }
     }
 
 
-    static FileInputStream getFile()
+    static FileInputStream getFile(String path)
     {
         FileInputStream reader = null;
+        LOGGER.info("File: {}", path.substring(path.lastIndexOf("\\")+1));
         try
         {
-            reader = new FileInputStream("E:\\Users\\Paul\\Downloads\\darling-in-the-franxx-ed5-escape-r2518.ms2mml");
+            reader = new FileInputStream(path);
         } catch (FileNotFoundException e)
         {
             e.printStackTrace();
@@ -60,8 +68,38 @@ public class TestMs2Mml
         return reader;
     }
 
+    static void viewZipFileContents(String path)
+    {
+        try (ZipFile file = new ZipFile(path))
+        {
+            //File temp = File.createTempFile("blah", "tmp");
+
+            Enumeration<? extends ZipEntry> entries = file.entries();
+            LOGGER.info("Zip File: {}", file.getName().substring(file.getName().lastIndexOf("\\")+1));
+            while(entries.hasMoreElements())
+            {
+                ZipEntry entry = entries.nextElement();
+                if (entry.isDirectory())
+                {
+                } else
+                {
+                    LOGGER.info("File: {}, size: {}", entry.getName(), entry.getSize());
+                    InputStream is = file.getInputStream(entry);
+                    test(is);
+                }
+            }
+            //temp.delete();
+        } catch (IOException|JAXBException e)
+        {
+            LOGGER.error(e);
+        }
+        LOGGER.info("----");
+    }
+
     public static void main(String[] args) throws Exception
     {
-        test();
+        test(getFile("E:\\Users\\Paul\\Downloads\\darling-in-the-franxx-ed5-escape-r2518.ms2mml"));
+        viewZipFileContents("E:\\Users\\Paul\\Downloads\\undertale-megalovania-r48.zip");
+        viewZipFileContents("E:\\Users\\Paul\\Downloads\\kingdom-hearts-3-dont-think-twice-chikai-kyle-landry-version.zip");
     }
 }
