fb5184cb93467b95180b538bf7089abd1c1b73fe
Aeronica <pmboese@gmail.com>
2019-03-05 07:40:21 -0600

    Default listener range set back to 24.

--- src/main/java/net/aeronica/mods/mxtune/config/ModConfig.java
+++ src/main/java/net/aeronica/mods/mxtune/config/ModConfig.java
@@ -107,7 +107,7 @@ public class ModConfig
             @Name("Listener Range")
             @LangKey("config.mxtune.listenerRange")
             @RangeInt(min=10, max=64)
-            public int listenerRange = 48;
+            public int listenerRange = 24;
 
             @Name("Group Play Abort Distance")
             @LangKey("config.mxtune.groupPlayAbortDistance")
