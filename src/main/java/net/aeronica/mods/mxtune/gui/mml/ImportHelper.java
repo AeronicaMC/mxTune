9ae37c0657d288e0c5920ab9ef4d9e4405d1d5f0
Aeronica <pmboese@gmail.com>
2019-05-29 16:54:58 -0500

    Continue work on MML file importing: zipped ms2mml (multi-part)

--- src/main/java/net/aeronica/mods/mxtune/gui/mml/ImportHelper.java
+++ src/main/java/net/aeronica/mods/mxtune/gui/mml/ImportHelper.java
@@ -23,12 +23,18 @@ import net.aeronica.mods.mxtune.caches.FileHelper;
 import net.aeronica.mods.mxtune.mxt.MXTuneFile;
 import net.aeronica.mods.mxtune.mxt.MXTunePart;
 import net.aeronica.mods.mxtune.mxt.MXTuneStaff;
+import net.aeronica.mods.mxtune.util.ModLogger;
 
 import javax.annotation.Nullable;
+import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Locale;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
 
 public class ImportHelper
 {
@@ -45,8 +51,8 @@ public class ImportHelper
                 return importMs2mml(path);
             case "mml":
                 break;
-            case "zip":
-                break;
+            case "zip": // Only multi-part ms2mml supported at this time
+                return importZippedMs2mml(path);
             default:
                 return null;
         }
@@ -58,6 +64,11 @@ public class ImportHelper
         return Files.getFileExtension(path.getFileName().toString().toLowerCase(Locale.ROOT));
     }
 
+    private static boolean hasExtension(String test, String ext)
+    {
+        return Files.getFileExtension(test).toLowerCase(Locale.ROOT).equals(ext);
+    }
+
     @Nullable
     private static MXTuneFile importMs2mml(Path path)
     {
@@ -65,16 +76,9 @@ public class ImportHelper
         if (ms2MmlReader.parseFile(path))
         {
             String title = FileHelper.removeExtension(path.getFileName().toString());
-            List<MXTuneStaff> staves = new ArrayList<>();
+            List<MXTuneStaff> staves = getStaves(ms2MmlReader.getMML());
+            if (staves.isEmpty()) return null;
             MXTuneFile mxTuneFile = new MXTuneFile();
-            int i = 0;
-            for (String mml : ms2MmlReader.getMML().replaceAll("MML@|;", "").split(","))
-            {
-                if (i < MAX_STAVES)
-                    staves.add(new MXTuneStaff(i++, mml));
-                else
-                    break;
-            }
             mxTuneFile.setTitle(title);
             MXTunePart part = new MXTunePart("Acoustic Piano", "", 0, staves);
             mxTuneFile.getParts().add(part);
@@ -82,5 +86,61 @@ public class ImportHelper
             return mxTuneFile;
         } else
             return null;
+    }
+
+    @Nullable
+    private static MXTuneFile importZippedMs2mml(Path path)
+    {
+        if (path == null) return null;
+        try (ZipFile file = new ZipFile(path.toString()))
+        {
+            String title = FileHelper.removeExtension(path.getFileName().toString());
+            MXTuneFile mxTuneFile = new MXTuneFile();
+            mxTuneFile.setTitle(title);
+            Enumeration<? extends ZipEntry> entries = file.entries();
+            ModLogger.debug("---- Zip File: %s", file.getName().substring(file.getName().lastIndexOf("\\")+1));
+            int count = 0;
+            while(entries.hasMoreElements())
+            {
+                ZipEntry entry = entries.nextElement();
+                if (!entry.isDirectory() && hasExtension(entry.getName(), "ms2mml"))
+                {
+                    Ms2MmlReader ms2MmlReader = new Ms2MmlReader();
+                    ModLogger.debug("Ext: %s, File: %s, size: %s", Files.getFileExtension(entry.getName()), entry.getName(), entry.getSize());
+                    InputStream is = file.getInputStream(entry);
+                    if (ms2MmlReader.parseStream(is))
+                    {
+                        title = FileHelper.removeExtension(entry.getName());
+                        List<MXTuneStaff> staves = getStaves(ms2MmlReader.getMML());
+                        if (!staves.isEmpty())
+                        {
+                            MXTunePart part = new MXTunePart("Acoustic Piano", title, 0, staves);
+                            mxTuneFile.getParts().add(part);
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count > 0 ? mxTuneFile : null;
+
+        } catch (IOException e)
+        {
+            ModLogger.error(e);
+        }
+        return null;
+    }
+
+    private static List<MXTuneStaff> getStaves(String data)
+    {
+        final List<MXTuneStaff> staves = new ArrayList<>();
+        int i = 0;
+        for (String mml : data.replaceAll("MML@|;", "").split(","))
+        {
+            if (i < MAX_STAVES)
+                staves.add(new MXTuneStaff(i++, mml));
+            else
+                break;
+        }
+        return staves;
     }
 }
