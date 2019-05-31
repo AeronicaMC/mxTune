eaf1693e66c7dbaff36403e5a2995850ffbb5aa4
Aeronica <pmboese@gmail.com>
2019-05-30 20:28:43 -0500

    Fix NPE because... DOH, Da array be too small! Limit to 10 tracks.

--- src/main/java/net/aeronica/libs/mml/readers/mml3mle/MMLFile.java
+++ src/main/java/net/aeronica/libs/mml/readers/mml3mle/MMLFile.java
@@ -170,7 +170,7 @@ public final class MMLFile
         for (Extension3mleTrack track : trackList)
         {
             int program = track.getInstrument() - 1; // 3MLEのInstruments番号は1がスタート.
-            String text[] = new String[]{"", "", ""};
+            String text[] = new String[MMLUtil.MAX_TRACKS];
             List<MXTuneStaff> staves = new ArrayList<>();
             for (int i = 0; i < track.getTrackCount(); i++)
             {
