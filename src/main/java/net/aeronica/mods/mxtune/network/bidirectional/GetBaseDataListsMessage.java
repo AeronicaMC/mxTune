87fb45e0e3791f1829fc40836bfb1b3a28457516
Aeronica <pmboese@gmail.com>
2019-06-15 11:55:16 -0500

    Remove redundant cast per IntelliJ.

--- src/main/java/net/aeronica/mods/mxtune/network/bidirectional/GetBaseDataListsMessage.java
+++ src/main/java/net/aeronica/mods/mxtune/network/bidirectional/GetBaseDataListsMessage.java
@@ -101,7 +101,7 @@ public class GetBaseDataListsMessage<E extends BaseData> extends AbstractMessage
             // Serialize data object to a byte array
             ByteArrayOutputStream bos = new ByteArrayOutputStream();
             ObjectOutputStream out = new ObjectOutputStream(bos);
-            out.writeObject((Serializable) listBaseData);
+            out.writeObject(listBaseData);
             out.close();
 
             // Get the bytes of the serialized object
