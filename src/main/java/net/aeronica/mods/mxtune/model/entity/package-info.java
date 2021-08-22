90edeabdef913d600e833769aae4881b841557a9
Aeronica <pmboese@gmail.com>
2021-08-22 14:04:08 -0500

    Break Everything

--- src/main/java/net/aeronica/mods/mxtune/model/entity/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package net.aeronica.mods.mxtune.model.entity;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/net/aeronica/mods/mxtune/model/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package net.aeronica.mods.mxtune.model;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
