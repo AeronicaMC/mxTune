d493c09f77e028a068983459da35f6ae2eb738cb
Aeronica <pmboese@gmail.com>
2021-10-03 08:05:19 -0500

    Toplevel datagen framework

---
+++ src/main/java/aeronicamc/mods/mxtune/datagen/ModDataProviders.java
@@ -0,0 +1,31 @@
+package aeronicamc.mods.mxtune.datagen;
+
+import aeronicamc.mods.mxtune.Reference;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+import static net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
+@Mod.EventBusSubscriber(modid = Reference.MOD_ID, bus = Bus.MOD)
+public class ModDataProviders
+{
+    @SubscribeEvent
+    public static void registerDataProviders(final GatherDataEvent event)
+    {
+        final DataGenerator dataGenerator = event.getGenerator();
+        final ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        if (event.includeClient())
+        {
+            // TODO
+        }
+
+        if (event.includeServer())
+        {
+            // TODO
+        }
+    }
+}
