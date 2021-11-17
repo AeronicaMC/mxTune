11f1dfe8e783cc3c9aedca3c97c8787c14f7d3d4
Aeronica <pmboese@gmail.com>
2021-11-17 15:09:46 -0600

    Update signatures to included long netTransitTime

--- src/main/java/aeronicamc/mods/mxtune/sound/ClientAudio.java
+++ src/main/java/aeronicamc/mods/mxtune/sound/ClientAudio.java
@@ -140,7 +140,7 @@ public class ClientAudio
      */
     public static void play(int secondsToSkip, long netTransitTime, int playID, int entityId, String musicText)
     {
-        play(playID, secondsToSkip, entityId, null, musicText, false, null);
+        play(secondsToSkip, netTransitTime, playID, entityId, null, musicText, false, null);
     }
 
     /**
@@ -151,12 +151,12 @@ public class ClientAudio
      */
     public static void play(Integer playID, BlockPos pos, String musicText)
     {
-        play(playID, 0, 0, pos, musicText, false, null);
+        play(0, 0, playID, 0, pos, musicText, false, null);
     }
 
     public static void playLocal(int playId, String musicText, @Nullable IAudioStatusCallback callback)
     {
-        play(playId, 0, 0 , mc.player == null ? null : mc.player.blockPosition(), musicText, true, callback);
+        play(0, 0, playId, 0 , mc.player == null ? null : mc.player.blockPosition(), musicText, true, callback);
     }
 
     // Determine if audio is 3D spacial or background
@@ -170,21 +170,22 @@ public class ClientAudio
 
     /**
      * The all-in-one #play(...) method of the {@link ClientAudio} class.
-     * @param playID The unique server identifier for each music submission. see {@link PlayIdSupplier}
      * @param secondsToSkip seconds to skip forward in the music.
+     * @param netTransitTime time in milliseconds for server packet to reach the client.
+     * @param playID The unique server identifier for each music submission. see {@link PlayIdSupplier}
      * @param entityId The unique entity id of the music source. Generally another player.
      * @param pos the {@link BlockPos} of a placed instrument of music machine in the world
      * @param musicText The MML {@link <A="https://en.wikipedia.org/wiki/MML">MML</A>} to be played
      * @param isClient if true, the local client player hears their own music in stereo else other players in 3D audio.
      * @param callback An optional callback that is fired when {@link Status} changes related to {@link AudioData}
      */
-    private static void play(int playID, int secondsToSkip, int entityId, @Nullable BlockPos pos, String musicText, boolean isClient, @Nullable IAudioStatusCallback callback)
+    private static void play(int secondsToSkip, long netTransitTime, int playID, int entityId, @Nullable BlockPos pos, String musicText, boolean isClient, @Nullable IAudioStatusCallback callback)
     {
         startThreadFactory();
         if(playID != PlayIdSupplier.INVALID)
         {
             addPlayIDQueue(playID);
-            AudioData audioData = new AudioData(secondsToSkip, 0, playID, pos, isClient, callback);
+            AudioData audioData = new AudioData(secondsToSkip, netTransitTime, playID, pos, isClient, callback);
             setAudioFormat(audioData);
             AudioData result = playIDAudioData.putIfAbsent(playID, audioData);
             if (result != null)
