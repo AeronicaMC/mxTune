380c73d1c03928c19dc4cfb5e9e770c01cd4d7dc
Aeronica <pmboese@gmail.com>
2021-11-24 23:33:51 -0600

    Add a scaled gui item icon to the instrument screen.

--- src/main/java/aeronicamc/mods/mxtune/gui/InstrumentScreen.java
+++ src/main/java/aeronicamc/mods/mxtune/gui/InstrumentScreen.java
@@ -2,6 +2,7 @@ package aeronicamc.mods.mxtune.gui;
 
 import aeronicamc.mods.mxtune.Reference;
 import aeronicamc.mods.mxtune.inventory.InstrumentContainer;
+import aeronicamc.mods.mxtune.util.SheetMusicHelper;
 import aeronicamc.mods.mxtune.util.SoundFontProxyManager;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
@@ -9,6 +10,7 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.client.gui.widget.button.Button;
 import net.minecraft.entity.player.PlayerInventory;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
@@ -21,6 +23,7 @@ public class InstrumentScreen extends ContainerScreen<InstrumentContainer>
     private final ResourceLocation GUI = new ResourceLocation(Reference.MOD_ID, "textures/gui/multi_inst_inventory.png");
     Button buttonChangeInstrument;
     int instIndex;
+    String musicKey = "";
 
     public InstrumentScreen(InstrumentContainer screenContainer, PlayerInventory inv, ITextComponent titleIn)
     {
@@ -41,21 +44,28 @@ public class InstrumentScreen extends ContainerScreen<InstrumentContainer>
         assert minecraft != null;
         assert minecraft.player != null;
 
-        buttonChangeInstrument = new Button(xPos, yPos, imageWidth - 24, 20, minecraft.player.getMainHandItem().getHoverName(), (open) ->
+        buttonChangeInstrument = new Button(xPos, yPos, imageWidth - 24, 20, inventory.getSelected().getHoverName(), (open) ->
                 minecraft.setScreen(new GuiMultiInstChooser(this)));
         this.addButton(buttonChangeInstrument);
-        buttonChangeInstrument.setMessage(new TranslationTextComponent(SoundFontProxyManager.getLangKeyName(minecraft.player.getMainHandItem().getMaxDamage())));
+        buttonChangeInstrument.setMessage(new TranslationTextComponent(SoundFontProxyManager.getLangKeyName(inventory.getSelected().getMaxDamage())));
     }
 
     @Override
     public void tick()
     {
         int newIndex;
+        String newMusicKey;
         super.tick();
-        if (mcPlayerOk(minecraft) && instIndex != (newIndex = Objects.requireNonNull(minecraft.player).getMainHandItem().getMaxDamage()))
+        if (instIndex != (newIndex = inventory.getSelected().getMaxDamage()))
         {
             instIndex = newIndex;
             updateInstrumentName();
+        }
+        ItemStack sheetMusic = SheetMusicHelper.getIMusicFromIInstrument(inventory.getSelected());
+        if (SheetMusicHelper.hasMusicText(sheetMusic) && !(newMusicKey = SheetMusicHelper.getMusicTextKey(sheetMusic)).equals(musicKey))
+        {
+            musicKey = newMusicKey;
+            updateSheetMusic();
         }
     }
 
@@ -64,8 +74,14 @@ public class InstrumentScreen extends ContainerScreen<InstrumentContainer>
     private void updateInstrumentName()
     {
         if (mcPlayerOk(minecraft))
-            buttonChangeInstrument.setMessage(new TranslationTextComponent(SoundFontProxyManager.getLangKeyName(Objects.requireNonNull(minecraft.player).getMainHandItem().getMaxDamage())));
+            buttonChangeInstrument.setMessage(new TranslationTextComponent(SoundFontProxyManager.getLangKeyName(inventory.getSelected().getMaxDamage())));
     }
+
+    private void updateSheetMusic()
+    {
+
+    }
+
 
     @Override
     public void removed()
@@ -88,12 +104,14 @@ public class InstrumentScreen extends ContainerScreen<InstrumentContainer>
     public void render(MatrixStack matrixStack , int mouseX, int mouseY, float partialTicks) {
         this.renderBackground(matrixStack);
         super.render(matrixStack, mouseX, mouseY, partialTicks);
+        ModGuiHelper.RenderGuiItemScaled(this.itemRenderer,
+            Objects.requireNonNull(Objects.requireNonNull(minecraft).player).getMainHandItem().getStack(),
+                getGuiLeft() + 51, getGuiTop() + 50, 2, true);
         this.renderTooltip(matrixStack, mouseX, mouseY);
     }
 
     @Override
     protected void renderLabels(MatrixStack matrixStack , int mouseX, int mouseY) {
-        //this.font.draw(matrixStack, menu.getName().getContents(), 10, 8, TextColorFg.DARK_GRAY);
         this.font.draw(matrixStack, this.title, 10, 72, TextColorFg.DARK_GRAY);
     }
 }
