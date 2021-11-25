8edf40a1d8fcb60dec700dcd4ba98bdf1cf2c0a5
Aeronica <pmboese@gmail.com>
2021-11-24 23:33:08 -0600

    Save and restore the matrix so scaling only affects this item render.

--- src/main/java/aeronicamc/mods/mxtune/gui/ModGuiHelper.java
+++ src/main/java/aeronicamc/mods/mxtune/gui/ModGuiHelper.java
@@ -8,9 +8,11 @@ public class ModGuiHelper
 {
     public static void RenderGuiItemScaled(ItemRenderer itemRenderer, ItemStack pStack, int posX, int posY, int scale, boolean onCenter)
     {
+        RenderSystem.pushMatrix();
         int xRel = (onCenter ? posX - 16*scale/2 : posX)/scale;
         int yRel = (onCenter ? posY - 16*scale/2 : posY)/scale;
         RenderSystem.scalef(scale, scale, 1F);
         itemRenderer.renderAndDecorateItem(pStack, xRel, yRel);
+        RenderSystem.popMatrix();
     }
 }
