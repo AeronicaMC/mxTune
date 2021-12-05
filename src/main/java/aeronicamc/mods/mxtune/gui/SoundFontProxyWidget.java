3557875f5fd5bb4cc520999a92d8ffcae6bc165a
Aeronica <pmboese@gmail.com>
2021-12-05 14:32:47 -0600

    Move ugly no param constructor and setters to parent.

--- src/main/java/aeronicamc/mods/mxtune/gui/SoundFontProxyWidget.java
+++ src/main/java/aeronicamc/mods/mxtune/gui/SoundFontProxyWidget.java
@@ -1,16 +1,10 @@
 package aeronicamc.mods.mxtune.gui;
 
-import aeronicamc.mods.mxtune.util.AntiNull;
 import aeronicamc.mods.mxtune.util.SoundFontProxy;
 import aeronicamc.mods.mxtune.util.SoundFontProxyManager;
 import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.audio.SimpleSound;
-import net.minecraft.client.gui.AbstractGui;
-import net.minecraft.client.renderer.BufferBuilder;
-import net.minecraft.client.renderer.Tessellator;
-import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.SoundEvents;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.ITextProperties;
@@ -23,40 +17,12 @@ public class SoundFontProxyWidget extends ModExtendedList<SoundFontProxyWidget.E
 
     public SoundFontProxyWidget()
     {
-        this(Minecraft.getInstance(), 1, 1, 1, 1, Minecraft.getInstance().font.lineHeight + 4, 1, AntiNull.nonNullInjected());
+        super();
     }
 
     public SoundFontProxyWidget(Minecraft minecraft, int pWidth, int pHeight, int pY0, int pY1, int pItemHeight, int pLeft, Consumer<Entry> selectCallback)
     {
         super(minecraft, pWidth, pHeight, pY0, pY1, pItemHeight, pLeft, selectCallback);
-        super.setLeftPos(pLeft);
-        super.setRenderTopAndBottom(false);
-        super.setRenderSelection(true);
-        super.setRenderBackground(true);
-    }
-
-    public void setLayout(int pWidth, int pHeight, int pY0, int pY1, int pLeft)
-    {
-        this.width = pWidth;
-        this.setRowWidth(pWidth);
-        this.setLeftPos(pLeft);
-        this.height = pHeight;
-        super.y0 = pY0;
-        super.y1 = pY1;
-        super.setRenderTopAndBottom(false);
-        super.setRenderSelection(true);
-        super.setRenderBackground(true);
-    }
-
-    public void setCallBack(Consumer<Entry> selectCallback)
-    {
-        this.selectCallback = selectCallback;
-    }
-
-    @Override
-    protected int getScrollbarPosition()
-    {
-        return x0 + width - 6;
     }
 
     public SoundFontProxyWidget init()
@@ -79,19 +45,9 @@ public class SoundFontProxyWidget extends ModExtendedList<SoundFontProxyWidget.E
     }
 
     @Override
-    protected void renderBackground(MatrixStack pMatrixStack)
+    public void setCallBack(Consumer<Entry> selectCallback)
     {
-        Tessellator tessellator = Tessellator.getInstance();
-        BufferBuilder bufferbuilder = tessellator.getBuilder();
-        this.minecraft.getTextureManager().bind(AbstractGui.BACKGROUND_LOCATION);
-        RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
-        float f = 32.0F;
-        bufferbuilder.begin(7, DefaultVertexFormats.POSITION_TEX_COLOR);
-        bufferbuilder.vertex((double)this.x0, (double)this.y1, 0.0D).uv((float)this.x0 / 32.0F, (float)(this.y1 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-        bufferbuilder.vertex((double)this.x1, (double)this.y1, 0.0D).uv((float)this.x1 / 32.0F, (float)(this.y1 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-        bufferbuilder.vertex((double)this.x1, (double)this.y0, 0.0D).uv((float)this.x1 / 32.0F, (float)(this.y0 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-        bufferbuilder.vertex((double)this.x0, (double)this.y0, 0.0D).uv((float)this.x0 / 32.0F, (float)(this.y0 + (int)this.getScrollAmount()) / 32.0F).color(32, 32, 32, 255).endVertex();
-        tessellator.end();
+        this.selectCallback = selectCallback;
     }
 
     public class Entry extends ModExtendedList.AbstractListEntry<Entry>
