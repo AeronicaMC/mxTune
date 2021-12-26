9becd5bb5a2a1d20e936a32d61d836b981b98b36
Aeronica <pmboese@gmail.com>
2021-12-26 17:07:41 -0600

    Remove unused global static final settings.

--- src/main/java/aeronicamc/mods/mxtune/Reference.java
+++ src/main/java/aeronicamc/mods/mxtune/Reference.java
@@ -16,21 +16,13 @@
  */
 package aeronicamc.mods.mxtune;
 
-import java.util.UUID;
-
 public class Reference
 {
     private Reference() {/* NOP */}
 
-    public static final UUID EMPTY_UUID = new UUID(0L, 0L);
-    //public static final GUID EMPTY_GUID = new GUID(0L,0L,0L,0L);
-    //public static final GUID NO_MUSIC_GUID = PlayList.emptyPlaylist().getGUID();
     public static final String MOD_ID = "mxtune";
     public static final String MOD_NAME = "mxTune";
     public static final String VERSION = "{@VERSION}";
-    //static final String ACCEPTED_MINECRAFT_VERSIONS = "[1.12.2,1.13)";
-    //static final String DEPENDENTS = "required-after:forge@[1.12.2-14.23.5.2802,)";
-    static final String UPDATE = "https://gist.githubusercontent.com/Aeronica/dbc2619e0011d5bdbe7a162d0c6aa82b/raw/update.json";
     static final String CERTIFICATE_FINGERPRINT = "999640c365a8443393a1a21df2c0ede9488400e9";
 
     public static final int MAX_MML_PART_LENGTH = 25000;
