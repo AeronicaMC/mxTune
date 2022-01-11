7916825230166490e5c2d6a93957b21c36b2610f
Aeronica <pmboese@gmail.com>
2022-01-10 22:37:58 -0600

    Test in-game gui overlay rendering.

--- src/main/java/aeronicamc/mods/mxtune/events/RenderEvents.java
+++ src/main/java/aeronicamc/mods/mxtune/events/RenderEvents.java
@@ -3,9 +3,16 @@ package aeronicamc.mods.mxtune.events;
 import aeronicamc.mods.mxtune.Reference;
 import aeronicamc.mods.mxtune.init.ModBlocks;
 import aeronicamc.mods.mxtune.init.ModItems;
+import aeronicamc.mods.mxtune.util.IInstrument;
+import aeronicamc.mods.mxtune.util.SheetMusicHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.event.entity.player.ItemTooltipEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -13,6 +20,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, value = Dist.CLIENT)
 public class RenderEvents
 {
+    private static final Minecraft mc = Minecraft.getInstance();
     @SubscribeEvent
     public static void event(ItemTooltipEvent event)
     {
@@ -20,5 +28,19 @@ public class RenderEvents
             event.getToolTip().add(new TranslationTextComponent("tooltip.mxtune.block_music.help").withStyle(TextFormatting.YELLOW));
         else if (event.getItemStack().getItem().equals(ModItems.MUSIC_PAPER.get()))
             event.getToolTip().add(new TranslationTextComponent("tooltip.mxtune.music_paper.help").withStyle(TextFormatting.YELLOW));
+    }
+
+    @SubscribeEvent
+    public static void event(RenderGameOverlayEvent.Post event)
+    {
+        PlayerEntity player = mc.player;
+        ItemStack itemStack = player.inventory.getSelected();
+
+        if (event.getType() == RenderGameOverlayEvent.ElementType.ALL && (mc.screen == null) && itemStack.getItem() instanceof IInstrument)
+        {
+            FontRenderer fontRenderer = Minecraft.getInstance().font;
+            ItemStack sheetMusic = SheetMusicHelper.getIMusicFromIInstrument(itemStack);
+            fontRenderer.drawShadow(event.getMatrixStack(), SheetMusicHelper.getFormattedMusicTitle(sheetMusic), 10, 10, -1);
+        }
     }
 }
