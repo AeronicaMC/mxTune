8bb0a43babb0d23143ba655085ec91acee89a0f1
Aeronica <pmboese@gmail.com>
2022-01-11 23:17:54 -0600

    Test overlay ideas using some borrowed toast rendering code.

--- src/main/java/aeronicamc/mods/mxtune/events/RenderEvents.java
+++ src/main/java/aeronicamc/mods/mxtune/events/RenderEvents.java
@@ -5,10 +5,13 @@ import aeronicamc.mods.mxtune.init.ModBlocks;
 import aeronicamc.mods.mxtune.init.ModItems;
 import aeronicamc.mods.mxtune.util.IInstrument;
 import aeronicamc.mods.mxtune.util.SheetMusicHelper;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.AbstractGui;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
@@ -20,7 +23,7 @@ import net.minecraftforge.fml.common.Mod;
 @Mod.EventBusSubscriber(modid = Reference.MOD_ID, value = Dist.CLIENT)
 public class RenderEvents
 {
-    private static final Minecraft mc = Minecraft.getInstance();
+    static final Minecraft mc = Minecraft.getInstance();
     @SubscribeEvent
     public static void event(ItemTooltipEvent event)
     {
@@ -30,17 +33,39 @@ public class RenderEvents
             event.getToolTip().add(new TranslationTextComponent("tooltip.mxtune.music_paper.help").withStyle(TextFormatting.YELLOW));
     }
 
+    static ResourceLocation TEXTURE = new ResourceLocation("textures/gui/toasts.png");
+    static int width = 160;
+    static int height = 32;
+    static int blitOffset = 0;
+
     @SubscribeEvent
     public static void event(RenderGameOverlayEvent.Post event)
     {
+        if (mc.player == null )
+            return;
+
         PlayerEntity player = mc.player;
         ItemStack itemStack = player.inventory.getSelected();
 
+        // borrow toast render for testing some ideast.
         if (event.getType() == RenderGameOverlayEvent.ElementType.ALL && (mc.screen == null) && itemStack.getItem() instanceof IInstrument)
         {
-            FontRenderer fontRenderer = Minecraft.getInstance().font;
             ItemStack sheetMusic = SheetMusicHelper.getIMusicFromIInstrument(itemStack);
-            fontRenderer.drawShadow(event.getMatrixStack(), SheetMusicHelper.getFormattedMusicTitle(sheetMusic), 10, 10, -1);
+            int offset = Math.max(mc.font.width(SheetMusicHelper.getFormattedMusicTitle(sheetMusic)) + 40, width);
+            MatrixStack pPoseStack = event.getMatrixStack();
+
+            mc.getTextureManager().bind(TEXTURE);
+            RenderSystem.color3f(1.0F, 1.0F, 1.0F);
+            blit(pPoseStack, 0, 0, 0, 0, width, height);
+            blit(pPoseStack, offset - width + 10, 0, 10, 0, width, height);
+
+            mc.font.draw(pPoseStack, SheetMusicHelper.getFormattedMusicTitle(sheetMusic), 30.0F, 7.0F, -11534256);
+            mc.font.draw(pPoseStack, SheetMusicHelper.getFormattedMusicDuration(sheetMusic), 30.0F, 17.0F, -11534256);
+            mc.getItemRenderer().renderAndDecorateItem(itemStack, 8, 8);
         }
+    }
+
+    static void blit(MatrixStack pMatrixStack, int pX, int pY, int pUOffset, int pVOffset, int pUWidth, int pVHeight) {
+        AbstractGui.blit(pMatrixStack, pX, pY, blitOffset, (float)pUOffset, (float)pVOffset, pUWidth, pVHeight, 256, 256);
     }
 }
