44684849a1b5ecc388b03c57e962505c87ea2dc9
Aeronica <pmboese@gmail.com>
2022-03-06 19:10:16 -0600

    Test overrides... yes they are there. JEI's fast rendering does the right, efficient thing and renders a single model. I will review my options once again and see if I can do this a different way. Metadata is dead and I should know better than force this onto the vanilla rendering pipeline.

--- src/main/java/aeronicamc/mods/mxtune/jei/MxtuneJEIPlugin.java
+++ src/main/java/aeronicamc/mods/mxtune/jei/MxtuneJEIPlugin.java
@@ -7,12 +7,20 @@ import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
 import mezz.jei.api.ingredients.subtypes.UidContext;
 import mezz.jei.api.registration.ISubtypeRegistration;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ItemModelMesher;
+import net.minecraft.client.renderer.model.IBakedModel;
+import net.minecraft.client.renderer.model.ItemOverride;
+import net.minecraft.client.renderer.model.ItemOverrideList;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 @JeiPlugin
 public class MxtuneJEIPlugin implements IModPlugin
 {
+    private static final Logger LOGGER = LogManager.getLogger(MxtuneJEIPlugin.class);
     /**
      * The unique ID for this mod plugin.
      * The namespace should be your mod's modId.
@@ -31,6 +39,13 @@ public class MxtuneJEIPlugin implements IModPlugin
     @Override
     public void registerItemSubtypes(ISubtypeRegistration registration)
     {
+        ItemModelMesher itemModelMesher = Minecraft.getInstance().getItemRenderer().getItemModelShaper();
+        IBakedModel bakedModel= itemModelMesher.getItemModel(ModItems.MULTI_INST.get());
+        if (bakedModel != null)
+        {
+            ItemOverrideList list = bakedModel.getOverrides();
+            list.getOverrides().forEach((ItemOverride o) -> LOGGER.info("{}", o.getModel()));
+        }
         registration.registerSubtypeInterpreter(ModItems.MULTI_INST.get().getItem(), new MultiInstSubTypeInterpreter());
     }
 
