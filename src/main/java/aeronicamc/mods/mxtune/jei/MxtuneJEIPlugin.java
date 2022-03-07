c9affd814ab8fb261e0c66009e56921b35e66e67
Aeronica <pmboese@gmail.com>
2022-03-07 10:14:34 -0600

    Clean up and use a lamda instead of static class

--- src/main/java/aeronicamc/mods/mxtune/jei/MxtuneJEIPlugin.java
+++ src/main/java/aeronicamc/mods/mxtune/jei/MxtuneJEIPlugin.java
@@ -4,57 +4,23 @@ import aeronicamc.mods.mxtune.Reference;
 import aeronicamc.mods.mxtune.init.ModItems;
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
-import mezz.jei.api.ingredients.subtypes.IIngredientSubtypeInterpreter;
-import mezz.jei.api.ingredients.subtypes.UidContext;
 import mezz.jei.api.registration.ISubtypeRegistration;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.ItemModelMesher;
-import net.minecraft.client.renderer.model.IBakedModel;
-import net.minecraft.client.renderer.model.ItemOverride;
-import net.minecraft.client.renderer.model.ItemOverrideList;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 @JeiPlugin
 public class MxtuneJEIPlugin implements IModPlugin
 {
-    private static final Logger LOGGER = LogManager.getLogger(MxtuneJEIPlugin.class);
-    /**
-     * The unique ID for this mod plugin.
-     * The namespace should be your mod's modId.
-     */
     @Override
     public ResourceLocation getPluginUid()
     {
         return new ResourceLocation(Reference.MOD_ID);
     }
 
-    /**
-     * If your item has subtypes that depend on NBT or capabilities, use this to help JEI identify those subtypes correctly.
-     *
-     * @param registration
-     */
     @Override
     public void registerItemSubtypes(ISubtypeRegistration registration)
     {
-        ItemModelMesher itemModelMesher = Minecraft.getInstance().getItemRenderer().getItemModelShaper();
-        IBakedModel bakedModel= itemModelMesher.getItemModel(ModItems.MULTI_INST.get());
-        if (bakedModel != null)
-        {
-            ItemOverrideList list = bakedModel.getOverrides();
-            list.getOverrides().forEach((ItemOverride o) -> LOGGER.info("{}", o.getModel()));
-        }
-        registration.registerSubtypeInterpreter(ModItems.MULTI_INST.get().getItem(), new MultiInstSubTypeInterpreter());
-    }
-
-    public static class MultiInstSubTypeInterpreter implements IIngredientSubtypeInterpreter<ItemStack>
-    {
-        @Override
-        public String apply(ItemStack ingredient, UidContext context)
-        {
-            return Integer.toString(ingredient.getDamageValue());
-        }
+        registration.registerSubtypeInterpreter(
+                ModItems.MULTI_INST.get().getItem(),
+                (ingredient, context) -> Integer.toString(ingredient.getDamageValue()));
     }
 }
