4f6bdf7fc5e6376e338502f09c5f9208c5fd305e
Aeronica <pmboese@gmail.com>
2022-03-14 21:10:30 -0500

    Add a silly recipe for the accordion.

--- src/main/java/aeronicamc/mods/mxtune/datagen/MXTuneRecipeProvider.java
+++ src/main/java/aeronicamc/mods/mxtune/datagen/MXTuneRecipeProvider.java
@@ -2,11 +2,9 @@ package aeronicamc.mods.mxtune.datagen;
 
 import aeronicamc.mods.mxtune.Reference;
 import aeronicamc.mods.mxtune.init.ModItems;
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
-import net.minecraft.data.ShapelessRecipeBuilder;
+import net.minecraft.data.*;
 import net.minecraft.item.Items;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
@@ -29,6 +27,20 @@ public class MXTuneRecipeProvider extends RecipeProvider
                     .unlockedBy("has_paper", has(Items.PAPER))
                     .unlockedBy("has_black_dye", has(Tags.Items.DYES_BLACK))
                     .save(recipeConsumer);
+        }
+
+        {
+            ShapedRecipeBuilder.shaped(ModItems.MULTI_INST.get())
+                    .pattern("BBB")
+                    .pattern("SLS")
+                    .pattern("SSS")
+                    .define('B', Tags.Items.BONES)
+                    .define('S', Tags.Items.RODS_WOODEN)
+                    .define('L', Tags.Items.LEATHER)
+                    .unlockedBy("has_bone", has(Tags.Items.BONES))
+                    .unlockedBy("has_stick", has(Tags.Items.RODS_WOODEN))
+                    .unlockedBy("has_leather", has(Tags.Items.LEATHER))
+                    .save(recipeConsumer, new ResourceLocation(Reference.MOD_ID, "accordion_from_bones_sticks_and_leather"));
         }
     }
 
