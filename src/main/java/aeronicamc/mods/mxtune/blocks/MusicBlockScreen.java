4dc23307ab8c1a5d89bf14d1cd6b2f61b75d063e
Aeronica <pmboese@gmail.com>
2022-04-21 18:24:30 -0500

    WIP: updating MusicBlock. Port GuiRedstoneButton from mxTune 1.12.2 version.

--- src/main/java/aeronicamc/mods/mxtune/blocks/MusicBlockScreen.java
+++ src/main/java/aeronicamc/mods/mxtune/blocks/MusicBlockScreen.java
@@ -1,24 +1,57 @@
 package aeronicamc.mods.mxtune.blocks;
 
 import aeronicamc.mods.mxtune.Reference;
+import aeronicamc.mods.mxtune.gui.widget.list.GuiRedstoneButton;
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.gui.screen.inventory.ContainerScreen;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
 
 import java.util.Objects;
 
 public class MusicBlockScreen extends ContainerScreen<MusicBlockContainer>
 {
-    private final ResourceLocation GUI = new ResourceLocation(Reference.MOD_ID, "textures/gui/inv_music_block_gui.png");
+    public static final ResourceLocation GUI = new ResourceLocation(Reference.MOD_ID, "textures/gui/inv_music_block_gui.png");
+    private final GuiRedstoneButton backRSIn = new GuiRedstoneButton(GuiRedstoneButton.ArrowFaces.DOWN, p -> toggleBackRSIn());
+    private final GuiRedstoneButton leftRSOut = new GuiRedstoneButton(GuiRedstoneButton.ArrowFaces.LEFT, p -> toggleLeftRSOut());
+    private final GuiRedstoneButton rightSOut = new GuiRedstoneButton(GuiRedstoneButton.ArrowFaces.RIGHT, p -> toggleRightRSOut());
 
     public MusicBlockScreen(MusicBlockContainer screenContainer, PlayerInventory inv, ITextComponent titleIn)
     {
         super(screenContainer, inv, titleIn);
-        this.imageWidth = 176;
-        this.imageHeight = 180;
+        this.imageWidth = 180;
+        this.imageHeight = 176;
+    }
+
+    @Override
+    protected void init()
+    {
+        super.init();
+        backRSIn.setLayout(leftPos + 141, topPos + 12, 20, 20);
+        backRSIn.addHooverText(false, new StringTextComponent("Backside Redstone Input"));
+        leftRSOut.setLayout(leftPos + 131, topPos + 32, 20, 20);
+        rightSOut.setLayout(leftPos + 151, topPos + 32, 20, 20);
+        addButton(backRSIn);
+        addButton(leftRSOut);
+        addButton(rightSOut);
+    }
+
+    private void toggleBackRSIn()
+    {
+
+    }
+
+    private void toggleLeftRSOut()
+    {
+       // TODO:
+    }
+
+    private void toggleRightRSOut()
+    {
+        // TODO:
     }
 
     @Override
---
+++ src/main/java/aeronicamc/mods/mxtune/gui/widget/list/GuiRedstoneButton.java
@@ -0,0 +1,117 @@
+package aeronicamc.mods.mxtune.gui.widget.list;
+
+import aeronicamc.mods.mxtune.blocks.MusicBlockScreen;
+import aeronicamc.mods.mxtune.gui.widget.MXButton;
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.StringTextComponent;
+
+public class GuiRedstoneButton extends MXButton
+{
+    static final ITextComponent EMPTY = new StringTextComponent("");
+    private boolean signalEnabled;
+    private ArrowFaces direction;
+
+    public GuiRedstoneButton(ArrowFaces direction, IPressable pOnPress)
+    {
+        super(0, 0, 20, 20, EMPTY, pOnPress);
+        this.direction = direction;
+    }
+
+    public boolean isSignalEnabled()
+    {
+        return this.signalEnabled;
+    }
+
+    public void setSignalEnabled(boolean signalEnabledIn)
+    {
+        this.signalEnabled = signalEnabledIn;
+    }
+
+    @Override
+    public void renderButton(MatrixStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks) {
+        if (this.visible)
+        {
+            Minecraft.getInstance().getTextureManager().bind(MusicBlockScreen.GUI);
+            RenderSystem.color4f(1.0F, 1.0F, 1.0F, 1.0F);
+            GuiRedstoneButton.Icon guiRedstoneButtonIcon;
+            if (this.signalEnabled)
+            {
+                if (!this.active)
+                {
+                    guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_ENABLED_DISABLED;
+                }
+                else if (isHovered)
+                {
+                    guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_ENABLED_HOVER;
+                }
+                else
+                {
+                    guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_ENABLED;
+                }
+            }
+            else if (!this.active)
+            {
+                guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_DISABLED_DISABLED;
+            }
+            else if (isHovered)
+            {
+                guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_DISABLED_HOVER;
+            }
+            else
+            {
+                guiRedstoneButtonIcon = GuiRedstoneButton.Icon.SIGNAL_DISABLED_UNLOCKED;
+            }
+
+            this.blit(pMatrixStack, this.x, this.y, guiRedstoneButtonIcon.getX() + direction.getXOffset(), guiRedstoneButtonIcon.getY(), this.width, this.height);
+        }
+    }
+
+    public enum Icon
+    {
+        SIGNAL_ENABLED(0, 176),
+        SIGNAL_ENABLED_HOVER(0, 196),
+        SIGNAL_ENABLED_DISABLED(0, 216),
+        SIGNAL_DISABLED_UNLOCKED(20, 176),
+        SIGNAL_DISABLED_HOVER(20, 196),
+        SIGNAL_DISABLED_DISABLED(20, 216);
+
+        private final int x;
+        private final int y;
+
+        Icon(int xIn, int yIn)
+        {
+            this.x = xIn;
+            this.y = yIn;
+        }
+
+        public int getX()
+        {
+            return this.x;
+        }
+
+        public int getY()
+        {
+            return this.y;
+        }
+    }
+
+    public enum ArrowFaces
+    {
+        UP(0),
+        DOWN(40),
+        LEFT(80),
+        RIGHT(120);
+
+        private final int direction;
+
+        ArrowFaces(int directionIn)
+        {
+            this.direction = directionIn;
+        }
+
+        public int getXOffset() { return this.direction; }
+    }
+}
