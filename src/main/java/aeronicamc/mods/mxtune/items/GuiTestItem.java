72c00a18ccb3e80424d038222113feb64a8f780d
Aeronica <pmboese@gmail.com>
2022-05-20 18:08:03 -0500

    Make the GuiTestItem behave like a wrench.

--- src/main/java/aeronicamc/mods/mxtune/items/GuiTestItem.java
+++ src/main/java/aeronicamc/mods/mxtune/items/GuiTestItem.java
@@ -1,14 +1,15 @@
 package aeronicamc.mods.mxtune.items;
 
-import aeronicamc.mods.mxtune.caps.player.PlayerNexusProvider;
-import aeronicamc.mods.mxtune.caps.venues.MusicVenueProvider;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUseContext;
+import net.minecraft.tags.ITag;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -23,45 +24,72 @@ public class GuiTestItem extends Item
         super(properties);
     }
 
+    /**
+     * This is called when the item is used, before the block is activated.
+     *
+     * @param stack
+     * @param context
+     * @return Return PASS to allow vanilla handling, any other to skip normal code.
+     */
+    @Override
+    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context)
+    {
+        return ActionResultType.PASS;
+    }
+
     @Nonnull
     @Override
     public ActionResult<ItemStack> use(@Nonnull World worldIn, @Nonnull PlayerEntity playerIn, @Nonnull Hand handIn)
     {
-        if (!worldIn.isClientSide)
-        {
-            if (!playerIn.isShiftKeyDown())
-            {
-                //PacketDispatcher.sendTo(new OpenScreenMessage(OpenScreenMessage.SM.TEST_ONE), (ServerPlayerEntity) playerIn);
-                PlayerNexusProvider.getNexus(playerIn).ifPresent(p->{
-                        p.setPlayId(worldIn.getRandom().nextInt(10) + 5);
-                });
-                MusicVenueProvider.getMusicVenues(playerIn.level).ifPresent(s->{
-                    s.setInt((worldIn.getRandom().nextInt(50)) + 50);
-                });
-            }
-            else
-            {
-                //PacketDispatcher.sendTo(new OpenScreenMessage(OpenScreenMessage.SM.TEST_TWO), (ServerPlayerEntity) playerIn);
-            }
+        ItemStack itemstack = playerIn.getItemInHand(handIn);
+//        if (!worldIn.isClientSide)
+//        {
+//            if (!playerIn.isShiftKeyDown())
+//            {
+//                //PacketDispatcher.sendTo(new OpenScreenMessage(OpenScreenMessage.SM.TEST_ONE), (ServerPlayerEntity) playerIn);
+//                PlayerNexusProvider.getNexus(playerIn).ifPresent(p->{
+//                        p.setPlayId(worldIn.getRandom().nextInt(10) + 5);
+//                });
+//                MusicVenueProvider.getMusicVenues(playerIn.level).ifPresent(s->{
+//                    s.setInt((worldIn.getRandom().nextInt(50)) + 50);
+//                });
+//            }
+//            else
+//            {
+//                //PacketDispatcher.sendTo(new OpenScreenMessage(OpenScreenMessage.SM.TEST_TWO), (ServerPlayerEntity) playerIn);
+//            }
+//
+//        } else if (!playerIn.isShiftKeyDown())
+//        {
+//            // nop
+//        } else
+//        {
+//            PlayerNexusProvider.getNexus(playerIn).ifPresent(p->{
+//                LOGGER.debug("PlayerNexus: playId: {}", p.getPlayId());
+//            });
+//            MusicVenueProvider.getMusicVenues(playerIn.level).ifPresent(s->{
+//                LOGGER.debug("MusicVenus: someInt: {}, Venues {}", s.getInt(), s.getMusicVenues().size());
+//            });
+//        }
+        return ActionResult.pass(itemstack);
+    }
 
-        } else if (!playerIn.isShiftKeyDown())
-        {
-            // nop
-        } else
-        {
-            PlayerNexusProvider.getNexus(playerIn).ifPresent(p->{
-                LOGGER.debug("PlayerNexus: playId: {}", p.getPlayId());
-            });
-            MusicVenueProvider.getMusicVenues(playerIn.level).ifPresent(s->{
-                LOGGER.debug("MusicVenus: someInt: {}, Venues {}", s.getInt(), s.getMusicVenues().size());
-            });
-        }
-        return super.use(worldIn, playerIn, handIn);
+    @Override
+    public boolean is(ITag<Item> p_206844_1_)
+    {
+        return super.is(p_206844_1_);
     }
 
     @Override
     public ActionResultType useOn(ItemUseContext context)
     {
-        return super.useOn(context);
+        return ActionResultType.PASS;
+    }
+
+    // For a wrench where we want to use SHIFT-Right-Click to pick up a block this needs to return true.
+    @Override
+    public boolean doesSneakBypassUse(ItemStack stack, IWorldReader world, BlockPos pos, PlayerEntity player)
+    {
+        return true;
     }
 }
