86f7cd98fa73a49c58c818e6789183dfc0ed2d37
Aeronica <pmboese@gmail.com>
2022-05-23 11:10:26 -0500

    Fix dedicated server class load error related to particle factory registration. Doh :P I read the docs, but I messed up anyway. Correct Music Block in frame size.

---
+++ src/main/java/aeronicamc/mods/mxtune/init/ModBusClientRegistryEventHandler.java
@@ -0,0 +1,19 @@
+package aeronicamc.mods.mxtune.init;
+
+import aeronicamc.mods.mxtune.Reference;
+import aeronicamc.mods.mxtune.render.particles.SpeakerParticle;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid= Reference.MOD_ID, value= Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
+public class ModBusClientRegistryEventHandler
+{
+    @SubscribeEvent
+    public static void event(ParticleFactoryRegisterEvent event)
+    {
+        Minecraft.getInstance().particleEngine.register(ModParticles.SPEAKER_TYPE.get(), new SpeakerParticle.Factory());
+    }
+}
--- src/main/java/aeronicamc/mods/mxtune/init/ModParticles.java
+++ src/main/java/aeronicamc/mods/mxtune/init/ModParticles.java
@@ -1,38 +1,19 @@
 package aeronicamc.mods.mxtune.init;
 
 import aeronicamc.mods.mxtune.Reference;
-import aeronicamc.mods.mxtune.render.particles.SpeakerParticle;
-import net.minecraft.client.Minecraft;
 import net.minecraft.particles.BasicParticleType;
-import net.minecraft.particles.IParticleData;
 import net.minecraft.particles.ParticleType;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.RegistryObject;
-import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-@Mod.EventBusSubscriber(modid=Reference.MOD_ID, value= Dist.CLIENT, bus=Mod.EventBusSubscriber.Bus.MOD)
 public class ModParticles
 {
     private static final DeferredRegister<ParticleType<?>> PARTICLES = DeferredRegister.create(ForgeRegistries.PARTICLE_TYPES, Reference.MOD_ID);
     private static boolean isInitialized;
 
-    private static final RegistryObject<ParticleType<BasicParticleType>> SPEAKER_TYPE = PARTICLES.register("speaker", () -> new BasicParticleType(true));
-    // Make nice cast for Level#addParticle
-    public static IParticleData getSpeaker()
-    {
-        return ((BasicParticleType) SPEAKER_TYPE.get());
-    }
-
-    @SubscribeEvent
-    public static void event(ParticleFactoryRegisterEvent event)
-    {
-        Minecraft.getInstance().particleEngine.register(ModParticles.SPEAKER_TYPE.get(), new SpeakerParticle.Factory());
-    }
+    static final RegistryObject<ParticleType<BasicParticleType>> SPEAKER_TYPE = PARTICLES.register("speaker", () -> new BasicParticleType(true));
 
     /**
      * Registers the {@link DeferredRegister} instance with the mod event bus.
