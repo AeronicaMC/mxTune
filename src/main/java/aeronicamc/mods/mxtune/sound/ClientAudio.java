6334627084f461078dc2101a9243f3d23bd58002
Aeronica <pmboese@gmail.com>
2022-07-04 14:27:20 -0500

    ClientAudio refactor: Only poll source management twice per second to lessen load.

--- src/main/java/aeronicamc/mods/mxtune/sound/ClientAudio.java
+++ src/main/java/aeronicamc/mods/mxtune/sound/ClientAudio.java
@@ -410,8 +410,8 @@ public class ClientAudio
     {
         if (event.side == LogicalSide.CLIENT && event.phase == TickEvent.Phase.END)
         {
-            // one update 4 times per second
-            if (counter++ % 5 == 0)
+            // one update twice per second
+            if (counter++ % 10 == 0)
                 prioritizeAndLimitSources();
         }
     }
