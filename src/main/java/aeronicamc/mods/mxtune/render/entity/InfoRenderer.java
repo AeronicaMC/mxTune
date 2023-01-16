5028c7e93803fcc4f3902a90ff30657a545421f9
Aeronica <pmboese@gmail.com>
2023-01-16 13:14:00 -0600

    Add a color bar method for testing the info panel.

--- src/main/java/aeronicamc/mods/mxtune/render/entity/InfoRenderer.java
+++ src/main/java/aeronicamc/mods/mxtune/render/entity/InfoRenderer.java
@@ -49,7 +49,7 @@ public class InfoRenderer implements AutoCloseable
     public void updateInfoTexture(MusicVenueInfoEntity infoEntity) {
         InfoRenderer.Instance infoRendererInstance = this.infoRendererInstances.get(infoEntity.getId());
         if (infoRendererInstance != null)
-            infoRendererInstance.updateInfoTexture(infoEntity);
+            infoRendererInstance.updateInfoTexture();
     }
 
     public void updateAll()
@@ -59,7 +59,7 @@ public class InfoRenderer implements AutoCloseable
 
     private void createInfoRendererInstance(MusicVenueInfoEntity infoEntity) {
         InfoRenderer.Instance infoRendererInstance = new InfoRenderer.Instance(infoEntity);
-        infoRendererInstance.updateInfoTexture(infoEntity);
+        infoRendererInstance.updateInfoTexture();
         this.infoRendererInstances.put(infoEntity.getId(), infoRendererInstance);
     }
 
@@ -82,44 +82,82 @@ public class InfoRenderer implements AutoCloseable
 
     class Instance implements AutoCloseable
     {
-        private final MusicVenueInfoEntity infoEntity;
         private final DynamicTexture infoTexture;
         private final RenderType renderType;
         private final Random random = new Random();
+        private final int entityId;
+        private final int texWidth;
+        private final int texHeight;
 
-        private final int width;
-        private final int height;
-
-        private Instance(MusicVenueInfoEntity infoEntity)
+        private Instance(MusicVenueInfoEntity pInfoEntity)
         {
-            this.infoEntity = infoEntity;
-            this.width = infoEntity.getWidth()*4;
-            this.height = infoEntity.getHeight()*4;
-            this.infoTexture = new DynamicTexture(this.width, this.height, true);
-            ResourceLocation dynamicTextureLocation = InfoRenderer.this.textureManager.register("info/" + infoEntity.getId(), this.infoTexture);
+            this.entityId = pInfoEntity.getHeight();
+            this.texWidth = pInfoEntity.getWidth() * 4;
+            this.texHeight = pInfoEntity.getHeight() * 4;
+            this.infoTexture = new DynamicTexture(this.texWidth, this.texHeight, true);
+            ResourceLocation dynamicTextureLocation = InfoRenderer.this.textureManager.register("info/" + pInfoEntity.getId(), this.infoTexture);
             this.renderType = RenderType.text(dynamicTextureLocation);
         }
 
-        private void updateInfoTexture(MusicVenueInfoEntity infoEntity)
-        {
-            for(int pixelY = 0; pixelY < this.height; pixelY++) {
-                for(int pixelX = 0; pixelX < this.width; pixelX++) {
-                    int color = randomColor();
-                    this.infoTexture.getPixels().setPixelRGBA(pixelX, pixelY, color);
-                }
-            }
-            this.infoTexture.upload();
+        private void updateInfoTexture() {
+            // noiseFill();
+            colorBars();
         }
 
-        private void updateInfoTexture()
-        {
-            for(int pixelY = 0; pixelY < this.height; pixelY++) {
-                for(int pixelX = 0; pixelX < this.width; pixelX++) {
-                    int color = randomColor();
-                    this.infoTexture.getPixels().setPixelRGBA(pixelX, pixelY, color);
+        private void noiseFill() {
+            if (this.infoTexture.getPixels() != null) {
+                for (int pixelY = 0; pixelY < this.texHeight; pixelY++) {
+                    for (int pixelX = 0; pixelX < this.texWidth; pixelX++) {
+                        int color = randomColor();
+                        this.infoTexture.getPixels().setPixelRGBA(pixelX, pixelY, color);
+                    }
                 }
+                this.infoTexture.upload();
             }
-            this.infoTexture.upload();
+        }
+
+        int[][] upperBars = {
+                { 104, 104, 104 },  // 40% gray
+                { 180, 180, 180 },  // 75% white
+                { 180, 180, 16  },  // 75% yellow
+                { 16,  180, 180 },  // 75% cyan
+                { 16,  180, 16  },  // 75% green
+                { 180, 16,  180 },  // 75% magenta
+                { 180, 16,  16  },  // 75% red
+                { 16,  16,  180 },  // 75% blue
+        };
+
+        int[][] lowerBars = {
+                { 16,  16,  16  },  // 40% black
+                { 235, 235, 235 },  // 100% white
+                { 72,  16,  118 },  // +Q
+                { 106, 52,  16  },  // +I
+                { 16,  70,  106 },  // -I
+                { 0,   0,   0   },  // FULL black
+                { 255, 255, 255 },  // FULL white
+                { 255, 255, 0   },  // FULL yellow
+        };
+
+        private void colorBars() {
+            int barWidth = this.texWidth / 8;
+            int upperBarHeight = (this.texHeight / 8) * 6;
+            if (this.infoTexture.getPixels() != null) {
+                int x = 0;
+                for (int xBar = 0; xBar < 8; xBar++) {
+                    int color = getABGR(upperBars[xBar][0], upperBars[xBar][1], upperBars[xBar][2]);
+                    this.infoTexture.getPixels().fillRect(x, 0, barWidth , upperBarHeight, color);
+
+                    color = getABGR(lowerBars[xBar][0], lowerBars[xBar][1], lowerBars[xBar][2]);
+                    this.infoTexture.getPixels().fillRect(x, upperBarHeight, barWidth , texHeight - upperBarHeight, color);
+                    x += barWidth;
+                }
+                this.infoTexture.upload();
+            }
+        }
+
+        private int getABGR(int r, int g, int b)
+        {
+            return  ((r & 0x000000FF) | ((g & 0x000000FF) << 8) | ((b & 0x000000FF) << 16) | (0xFF000000));
         }
 
         private int randomColor()
@@ -134,6 +172,21 @@ public class InfoRenderer implements AutoCloseable
         private IVertexBuilder getInfoRenderVertexBuilder(IRenderTypeBuffer renderTypeBuffer)
         {
             return renderTypeBuffer.getBuffer(this.renderType);
+        }
+
+        private int getEntityId()
+        {
+            return entityId;
+        }
+
+        private int getTexWidth()
+        {
+            return texWidth;
+        }
+
+        private int getTexHeight()
+        {
+            return texHeight;
         }
 
         @Override
