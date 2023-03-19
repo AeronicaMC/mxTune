14319a1c55eabfab841635283a7100a3ba51e04f
Aeronica <pmboese@gmail.com>
2023-03-19 16:48:44 -0500

    Testing player interactions with other players. Back door join group :D

--- src/main/java/aeronicamc/mods/mxtune/events/ModCommonEvents.java
+++ src/main/java/aeronicamc/mods/mxtune/events/ModCommonEvents.java
@@ -7,11 +7,16 @@ import aeronicamc.mods.mxtune.blocks.LockableHelper;
 import aeronicamc.mods.mxtune.entity.MusicSourceEntity;
 import aeronicamc.mods.mxtune.entity.MusicVenueInfoEntity;
 import aeronicamc.mods.mxtune.init.ModTags;
+import aeronicamc.mods.mxtune.managers.Group;
+import aeronicamc.mods.mxtune.managers.GroupManager;
 import aeronicamc.mods.mxtune.managers.PlayManager;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Hand;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.event.entity.EntityEvent;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
@@ -58,17 +63,52 @@ public class ModCommonEvents
     @SubscribeEvent
     public static void event(PlayerInteractEvent.EntityInteractSpecific event)
     {
-        if(!event.getEntityLiving().getCommandSenderWorld().isClientSide())
+        if(!event.getWorld().isClientSide() && initiatorPredicate(event) && targetPredicate(event) && hasInstrument(event.getItemStack()))
         {
-            LOGGER.debug("Source: {}, Target: {}", event.getPlayer().getDisplayName().getString(), event.getTarget().getDisplayName().getString());
-            LOGGER.debug("isInstrument? {}, Part: {}", hasInstrument(event.getItemStack()), event.getLocalPos());
+            PlayerEntity initiator = (PlayerEntity) event.getEntityLiving();
+            PlayerEntity target = (PlayerEntity) event.getTarget();
+            Group iniGroup = GroupManager.getGroup(initiator.getId());
+            Group tarGroup = GroupManager.getGroup(target.getId());
+            LOGGER.debug("Initiator: {} right-clicked Target: {} with a {}. Is it an Instrument? {}",
+                         initiator.getDisplayName().getString(),
+                         target.getDisplayName().getString(),
+                         itemName(event.getItemStack()), hasInstrument(event.getItemStack()) ? "Yes" : "No");
+            if (!tarGroup.isEmpty() && tarGroup.getLeader() == target.getId() && iniGroup.isEmpty())
+            {
+                initiator.sendMessage(new TranslationTextComponent("chat.mxtune.groupManager.requests_to_join", target.getName()), target.getUUID());
+                LOGGER.debug("{} Requests to join group", initiator.getDisplayName().getString());
+                GroupManager.addMember(tarGroup.getGroupId(), initiator); // TODO this is a test only. Adding self to targets group is a no-no :P
+            }
+            else if (!tarGroup.isEmpty())
+            {
+                if (tarGroup.getLeader() != target.getId() && iniGroup.isEmpty())
+                    target.sendMessage(new TranslationTextComponent("chat.mxtune.groupManager.player_not_leader", target.getName()), target.getUUID());
+                else
+                    target.sendMessage(new TranslationTextComponent("chat.mxtune.groupManager.cannot_join_if_group_member", target.getName()), target.getUUID());
+            }
         }
+    }
+
+    private static boolean initiatorPredicate(PlayerInteractEvent.EntityInteractSpecific event)
+    {
+        return event.getEntityLiving() instanceof PlayerEntity && event.getHand().equals(Hand.MAIN_HAND);
+    }
+
+    private static boolean targetPredicate(PlayerInteractEvent.EntityInteractSpecific event)
+    {
+        return event.getTarget() instanceof PlayerEntity;
     }
 
     private static boolean hasInstrument(ItemStack itemStack)
     {
-        return itemStack.getItem().is(ModTags.Items.INSTRUMENTS);
+        return itemStack.getItem().is(ModTags.Items.INSTRUMENTS) || itemStack.getItem().is(ModTags.Items.MUSIC_MACHINES);
     }
+
+    private static String itemName(ItemStack itemStack)
+    {
+        return itemStack.getHoverName().getString();
+    }
+
 
     // Test if a player can break this block with a BE of type ILockable.
     // Only the owner of the block can break these.
