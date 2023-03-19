523e73ce3be0dec2ba835f46b2cb88a3bd3ad243
Aeronica <pmboese@gmail.com>
2023-03-19 16:47:58 -0500

    Don't open gui when clicking on LivingEntity

--- src/main/java/aeronicamc/mods/mxtune/items/MusicPaperItem.java
+++ src/main/java/aeronicamc/mods/mxtune/items/MusicPaperItem.java
@@ -2,10 +2,12 @@ package aeronicamc.mods.mxtune.items;
 
 import aeronicamc.mods.mxtune.gui.Handler;
 import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
@@ -43,5 +45,12 @@ public class MusicPaperItem extends Item
     public int getUseDuration(ItemStack pStack) // getMaxItemUseDuration
     {
         return 72000;
+    }
+
+    // Don't open gui when clicking on LivingEntity
+    @Override
+    public ActionResultType interactLivingEntity(ItemStack pStack, PlayerEntity pPlayer, LivingEntity pTarget, Hand pHand)
+    {
+        return ActionResultType.SUCCESS;
     }
 }
