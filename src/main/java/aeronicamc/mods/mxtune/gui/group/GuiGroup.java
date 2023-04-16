7e8ee7ebac10d1c75154037854b18c356f3d115a
Aeronica <pmboese@gmail.com>
2023-04-15 20:40:21 -0500

    Replace GroupMemberList with a simpler list of members with buttons.

--- src/main/java/aeronicamc/mods/mxtune/gui/group/GuiGroup.java
+++ src/main/java/aeronicamc/mods/mxtune/gui/group/GuiGroup.java
@@ -4,6 +4,7 @@ import aeronicamc.mods.mxtune.gui.MXScreen;
 import aeronicamc.mods.mxtune.gui.TextColorFg;
 import aeronicamc.mods.mxtune.gui.widget.MXButton;
 import aeronicamc.mods.mxtune.gui.widget.MXTextFieldWidget;
+import aeronicamc.mods.mxtune.managers.Group;
 import aeronicamc.mods.mxtune.managers.GroupClient;
 import aeronicamc.mods.mxtune.network.PacketDispatcher;
 import aeronicamc.mods.mxtune.network.messages.GetGroupPinMessage;
@@ -29,27 +30,49 @@ public class GuiGroup extends MXScreen implements IGroupClientChangedCallback
     private final MXButton buttonDisband = new MXButton(p -> disband());
     private final MXTextFieldWidget groupDisplay = new MXTextFieldWidget(1024);
     private final MXTextFieldWidget pinDisplay = new MXTextFieldWidget(4);
-    private final List<MemberButtons> memberButtons = new ArrayList<>();
+    private final MemberDisplay memberDisplay;
     private int counter;
     private final int groupId;
     private int lastHash;
+    private final int lineHeight;
+    private final int nameWidth;
     public GuiGroup()
     {
         super(StringTextComponent.EMPTY);
         groupId = GroupClient.getGroup(player().getId()).getGroupId();
+        lineHeight = mc().font.lineHeight + 6;
+        nameWidth = mc().font.width("MMMMMMMMMMMM") + 4;
+        memberDisplay = new MemberDisplay(this);
+        GroupClient.setCallback(this);
+    }
+
+    private int getGroupId()
+    {
+        return this.groupId;
+    }
+
+    private int getLineHeight()
+    {
+        return this.lineHeight;
+    }
+
+    private int getNameWidth()
+    {
+        return this.nameWidth;
     }
 
     @Override
     protected void init()
     {
         super.init();
-        GroupClient.setCallback(this);
+
         PacketDispatcher.sendToServer(new GetGroupPinMessage());
-        initMemberDisplay(10, 10);
+        memberDisplay.initMemberDisplay(10, 10);
 
         int groupDisplayWidth = mc().font.width(GuiPin.getGroupLeaderInfo(groupDisplay, player(), groupId));
         int pinDisplayWidth = mc().font.width("0000");
         groupDisplay.setLayout(width/2 - groupDisplayWidth /2, 10, groupDisplayWidth + 8, 20);
+        groupDisplay.setBordered(false);
         ITextComponent msg = GuiPin.getGroupLeaderInfo(groupDisplay, player(), groupId);
         lastHash = msg.hashCode();
         groupDisplay.setValue(msg.getString());
@@ -71,14 +94,24 @@ public class GuiGroup extends MXScreen implements IGroupClientChangedCallback
         switch (type)
         {
             case Group:
+                this.reInit();
+                break;
             case Member:
-                this.init();
+                // this.updateStatus()
                 break;
             case Pin:
                 pinDisplay.setValue(GroupClient.getPrivatePin());
                 break;
         }
         System.out.printf("callback: %s\n", type);
+    }
+
+    private void reInit()
+    {
+        this.buttons.clear();
+        this.children.clear();
+        this.setFocused(null);
+        this.init();
     }
 
     private void promote(Button button)
@@ -115,7 +148,7 @@ public class GuiGroup extends MXScreen implements IGroupClientChangedCallback
         this.fillGradient(pMatrixStack, 0, 0, this.width, this.height, -1072689136, -804253680);
         groupDisplay.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
         pinDisplay.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
-        memberButtons.forEach(p -> p.memberDraw(pMatrixStack, pMouseX, pMouseY, pPartialTicks));
+        memberDisplay.renderMemberDisplay(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
         super.render(pMatrixStack, pMouseX, pMouseY, pPartialTicks);
     }
 
@@ -164,80 +197,113 @@ public class GuiGroup extends MXScreen implements IGroupClientChangedCallback
         return Minecraft.getInstance();
     }
 
-    private void initMemberDisplay(int posX, int posY)
+    private static class MemberDisplay
     {
-        int y = posY;
-        memberButtons.clear();
-        for (Integer memberId : GroupClient.getGroupById(groupId).getMembers())
+        private final GuiGroup parent;
+        private final List<MemberInfo> memberButtons = new ArrayList<>();
+        int xPos;
+        int yPos;
+        final int maxWidth;
+        final int maxHeight;
+
+        MemberDisplay(GuiGroup parent)
         {
-            if (!GroupClient.isLeader(memberId))
+            this.parent = parent;
+            maxHeight = (this.parent.getLineHeight() * 17) + 4;
+            maxWidth = this.parent.getNameWidth() + 40;
+        }
+
+        private void initMemberDisplay(int posX, int posY)
+        {
+            this.xPos = posX;
+            this.yPos = posY + 2;
+            int y = this.yPos;
+            memberButtons.clear();
+            Group group = GroupClient.getGroupById(parent.getGroupId());
+            System.out.printf("Group Id %d, Leader Id: %d\n", parent.getGroupId(), group.getLeader());
+
+            y = leaderFirst(posX, y, group.getLeader());
+            for (Integer memberId : GroupClient.getGroupById(parent.getGroupId()).getMembers())
             {
-                MemberButtons memberButton = new MemberButtons(posX, y, memberId, this::promote, this::remove);
-                addButton(memberButton.promote);
-                addButton(memberButton.remove);
-                memberButtons.add(memberButton);
-                y += Member.height;
+                if (!GroupClient.isLeader(memberId))
+                {
+                    MemberInfo memberInfo = new MemberInfo(posX, y, parent, memberId, parent::promote, parent::remove);
+                    parent.addButton(memberInfo.promote);
+                    parent.addButton(memberInfo.remove);
+                    memberButtons.add(memberInfo);
+                    y += parent.getLineHeight();
+                }
             }
         }
-        System.out.printf("memberButtons size: %d\n", memberButtons.size());
+
+        private int leaderFirst(int posX, int y, int memberId)
+        {
+            MemberInfo memberInfo = new MemberInfo(posX, y, parent, memberId, parent::promote, parent::remove);
+            memberInfo.promote.active = false;
+            memberInfo.promote.visible = false;
+            memberInfo.remove.active = player().getId() == memberId;
+            memberInfo.remove.visible = player().getId() == memberId;
+            parent.addButton(memberInfo.promote);
+            parent.addButton(memberInfo.remove);
+            memberButtons.add(memberInfo);
+            return y + parent.getLineHeight();
+        }
+
+        private void renderMemberDisplay(MatrixStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks)
+        {
+            fill(pMatrixStack, this.xPos - 1, this.yPos - 1, this.xPos + this.maxWidth + 1, this.yPos + this.maxHeight + 1, -6250336);
+            fill(pMatrixStack, this.xPos, this.yPos, this.yPos + this.maxWidth - 2, this.yPos + this.maxHeight, -16777216);
+            memberButtons.forEach(p -> p.memberDraw(pMatrixStack, pMouseX, pMouseY, pPartialTicks));
+        }
     }
 
-    private static class MemberButtons
+    private static class MemberInfo
     {
-        static final ITextComponent PROMOTE = new StringTextComponent("^").withStyle(TextFormatting.GREEN, TextFormatting.BOLD);
-        static final ITextComponent REMOVE = new StringTextComponent("X").withStyle(TextFormatting.RED, TextFormatting.BOLD);
+        static final ITextComponent PROMOTE = new StringTextComponent("▲").withStyle(TextFormatting.GREEN, TextFormatting.BOLD); // '▲' dec: 9650 hex: 25B2 BLACK UP-POINTING TRIANGLE
+        static final ITextComponent REMOVE = new StringTextComponent("x").withStyle(TextFormatting.RED, TextFormatting.BOLD); // 'x'
         final int xPos;
         final int yPos;
+        private final int nameWidth;
+        private final int lineHeight;
         final MXButton promote;
         final MXButton remove;
-        final Member member;
+        final int memberId;
+        final ITextComponent name;
 
-        MemberButtons(int xPos, int yPos, int memberId, Button.IPressable pPromote, Button.IPressable pRemove)
+        MemberInfo(int xPos, int yPos, GuiGroup parent, int memberId, Button.IPressable pPromote, Button.IPressable pRemove)
         {
             this.xPos = xPos;
             this.yPos = yPos;
-            member = new Member(memberId, Objects.requireNonNull(player().level.getEntity(memberId)).getDisplayName());
+            this.nameWidth = parent.nameWidth;
+            this.lineHeight = parent.lineHeight;
+            this.memberId = memberId;
+            this.name = player().level.getEntity(memberId) != null ? player().level.getEntity(memberId).getDisplayName() : new StringTextComponent(Integer.toString(memberId));
             promote = new MXButton(pPromote);
-            promote.setMessage(MemberButtons.PROMOTE);
-            promote.setLayout(xPos + Member.width, yPos - 2, 20, Member.height);
+            promote.setMessage(MemberInfo.PROMOTE);
+            promote.setLayout(xPos + nameWidth, yPos, 20, this.lineHeight);
             promote.setIndex(memberId);
             promote.active = GroupClient.isLeader(player().getId());
 
             remove = new MXButton(pRemove);
-            remove.setMessage(MemberButtons.REMOVE);
-            remove.setLayout(promote.x + 20, yPos - 2, 20, Member.height);
+            remove.setMessage(MemberInfo.REMOVE);
+            remove.setLayout(promote.x + 20, yPos, 20, this.lineHeight);
             remove.setIndex(memberId);
             remove.active = GroupClient.isLeader(player().getId()) || memberId == player().getId();
         }
 
+        public int getNameWidth()
+        {
+            return nameWidth;
+        }
+
+        public int getLineHeight()
+        {
+            return lineHeight;
+        }
+
         void memberDraw(MatrixStack pMatrixStack, int pMouseX, int pMouseY, float pPartialTicks)
         {
-            mc().font.draw(pMatrixStack, member.getName(), xPos, yPos, GroupClient.isLeader(member.getMemberId()) ? TextColorFg.YELLOW: TextColorFg.WHITE);
-        }
-    }
-
-    private static class Member
-    {
-        int memberId;
-        ITextComponent name;
-
-        Member(int memberId, ITextComponent name)
-        {
-            this.memberId = memberId;
-            this.name = name;
-        }
-
-        static int width = mc().font.width("MMMMMMMMMMMM") + 4;
-        static int height = mc().font.lineHeight + 4;
-
-        public int getMemberId()
-        {
-            return memberId;
-        }
-
-        public ITextComponent getName()
-        {
-            return name;
+            mc().font.draw(pMatrixStack, name, xPos + 2, yPos + 2, GroupClient.isLeader(memberId) ? TextColorFg.YELLOW: TextColorFg.WHITE);
         }
     }
 }
